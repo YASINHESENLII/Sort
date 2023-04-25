3c2a8645f51090ea29258e8fbae88e4369b62c42
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-26 03:29:16 +0400

    Sort

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> languages = new SortedList<int, string>();
+
+            languages.Add(2001, "C#");
+            languages.Add(1983, "C++");
+            languages.Add(1995, "Java");
+            languages.Add(1972, "C");
+            languages.Add(1992, "Python");
+
+            foreach (var item in languages)
+            {
+                Console.WriteLine("Praqramlasdirma dili:" + item.Value + "Yaranma tarixi:" + item.Key);
+            }
+        }
+    }
+}
